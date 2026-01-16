2697a03ba74eb25b80e84abf8c5f54bdd6116aa8
Lea Verou <lea@verou.me>
2026-01-16 11:14:05 -0500

    Test index files

---
+++ test/index.js
@@ -0,0 +1,6 @@
+import utilTests from "./util/index.js";
+
+export default {
+	name: "All nudeps tests",
+	tests: [utilTests],
+};
---
+++ test/util/index.js
@@ -0,0 +1,6 @@
+import extractPackageNameTests from "./extract-package-name.js";
+
+export default {
+	name: "util tests",
+	tests: [extractPackageNameTests],
+};
