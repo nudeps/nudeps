ec22734a7ab6e4f65c63f43aa80836283dade8f9
Lea Verou <lea@verou.me>
2026-01-16 12:12:51 -0500

    Remove unused code

--- src/map.js
+++ src/map.js
@@ -1,6 +1,4 @@
 import { Generator } from "@jspm/generator";
-import { ImportMap } from "@jspm/import-map";
-import { readFileSync } from "node:fs";
 import { readJSONSync } from "./util.js";
 
 // These help us parse an importmap JS and extract the actual import map JSON
@@ -76,34 +74,6 @@ export async function getImportMapJs (map) {
 	let stringified = typeof map === "string" ? map : JSON.stringify(map, null, "\t");
 	return `{${injectImportMapName}(${COMMENT_START}${stringified}${COMMENT_END});
 ${injectImportMapCode}}`;
-}
-
-export function readImportMap ({ map = "importmap.js" } = {}) {
-	let contents = readFileSync(map, "utf8");
-	if (!contents.includes(COMMENT_START) || !contents.includes(COMMENT_END)) {
-		throw new Error("Malformed import map");
-	}
-
-	let start = contents.indexOf(COMMENT_START) + COMMENT_START.length;
-	let end = contents.lastIndexOf(COMMENT_END);
-	let importMap = contents.slice(start, end);
-	let mapJson = JSON.parse(importMap);
-	return new ImportMap({ map: mapJson });
-}
-
-export function getPaths (map) {
-	let rootPaths = map.imports ? Object.values(map.imports) : [];
-	let scopePaths = map.scopes ? Object.values(Object.assign(...Object.values(map.scopes))) : [];
-	return new Set([...rootPaths, ...scopePaths]);
-}
-
-export function getTopLevelDirectories (map) {
-	let paths = [...getPaths(map)];
-	// No g flag is intentional: we want the first match!
-	let dirs = paths
-		.map(path => path.match(/(?<=node_modules\/)@[\w-.]+\/[\w-.]+/)?.[0])
-		.filter(Boolean);
-	return new Set(dirs);
 }
 
 async function installPackage (generator, name, target) {
